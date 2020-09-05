0f090f128c4268d9408276449d604e0e9a0ab058
risenforces <to@risen.dev>
2020-09-05 20:02:14 +0500

    add package alias

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,14 @@ import { baz } from '@baz'
 import { Boo } from '@boo'
 import { Zoo } from '@lib/zoo'
 
+import Select from '@my-react-select'
+import Creatable from '@my-react-select/creatable'
+
 console.log(foo)
 console.log(reexportedFoo)
 console.log(bar)
 console.log(baz)
 console.log(Boo)
 console.log(Zoo)
+console.log(Select)
+console.log(Creatable)
